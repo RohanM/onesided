597bc4a6720ab2d971278e382ae3ff16b2d06e73
Rohan Mitchell <rohan@rohanmitchell.com>
2023-08-02 13:44:36 +1000

    Add --speakers to output speaker sample

--- remix.py
+++ remix.py
@@ -8,6 +8,11 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--ds", help="CSV file with diarisation", required=True)
 parser.add_argument("--input", help="Input WAV file", required=True)
 parser.add_argument(
+    "--speakers",
+    action="store_true",
+    help="Generate audio with samples of all speakers",
+)
+parser.add_argument(
     "--include-speakers",
     nargs="+",
     help="Speakers to include, comma-separated",
@@ -19,8 +24,6 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-output_filename = args.input.replace(".wav", ".onesided.wav")
-
 
 def main():
     full_audio = AudioSegment.from_wav(args.input)
@@ -30,7 +33,14 @@ def main():
         speakers = get_speakers(reader, args.include_speakers, args.exclude_speakers)
         csvfile.seek(0)
 
-        segments = build_audio(reader, speakers, full_audio)
+        if not args.speakers:
+            segments = build_audio(reader, speakers, full_audio)
+            output_filename_suffix = ".onesided.wav"
+        else:
+            segments = build_speakers_sample(reader, speakers, full_audio)
+            output_filename_suffix = ".speakers.wav"
+
+        output_filename = args.input.replace(".wav", output_filename_suffix)
         segments.export(output_filename, format="wav")
 
 
@@ -48,6 +58,30 @@ def build_audio(csv_reader, speakers, full_audio):
 
             print(f"{start / float(len(full_audio))*100:.0f}%")
             segments += full_audio[start:end]
+
+    return segments
+
+
+def build_speakers_sample(csv_reader, speakers, full_audio):
+    segments = AudioSegment.empty()
+
+    chime = AudioSegment.from_wav("audio/chime.wav")
+
+    for tgt_speaker in sorted(speakers):
+        for start, end, speaker in csv_reader:
+            start = float(start) * 1000
+            end = float(end) * 1000
+
+            # Fix clipping of first sample
+            if start < 500:
+                start = 0
+
+            if speaker == tgt_speaker and end - start > 1000:
+                segments += chime
+                segments += AudioSegment.from_wav(f"audio/num-{tgt_speaker[-2:]}.wav")
+
+                segments += full_audio[start:end]
+                break
 
     return segments
 
