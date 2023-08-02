95ef5057b9d42e4bc00e1c70e0c98f82ab3e990d
Rohan Mitchell <rohan@rohanmitchell.com>
2023-08-02 10:58:05 +1000

    Set output filename for diarisation

--- diarize.py
+++ diarize.py
@@ -1,19 +1,20 @@
 import sys
 from pyannote.audio import Pipeline
 
+input_filename = sys.argv[1]
+output_filename = input_filename.replace(".wav", ".ds.csv")
+
 print("Loading pipeline...")
 pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization",
     use_auth_token="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",
 )
 
-print(f"Opening audio file {sys.argv[1]}...")
-file = {"uri": "xxx", "audio": sys.argv[1]}
-dz = pipeline(file)
+print(f"Opening audio file {input_filename}...")
+dz = pipeline(input_filename)
 
-print("Reading diarization...")
-with open("dz.csv", "w") as f:
+print("Writing diarization...")
+with open(output_filename, "w") as f:
     for turn, _, speaker in dz.itertracks(yield_label=True):
         print(f"{turn.start},{turn.end},{speaker}")
         f.write(f"{turn.start},{turn.end},{speaker}\n")
-
