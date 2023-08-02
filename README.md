b29cd29c1ee90bef4a5d5110cb8dfd61361c17ef
Rohan Mitchell <rohan@rohanmitchell.com>
2023-08-02 12:02:01 +1000

    Take args from argparser

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ python diarise.py my-podcast.wav
 Produce sample audio to determine the identity of each speaker:
 
 ```shell
-python remix.py --speakers --ds my-podcast.ds.csv --in my-podcast.wav
+python remix.py --speakers --ds my-podcast.ds.csv --input my-podcast.wav
 # outputs my-podcast.speakers.wav
 ```
 
@@ -46,10 +46,10 @@ Output the processed audio:
 
 ```shell
 # Include only specified speakers
-python remix.py --ds my-podcast.ds.csv --in my-podcast.wav --include-speakers=1,3
+python remix.py --ds my-podcast.ds.csv --input my-podcast.wav --include-speakers=1,3
 
 # Exclude some speakers, retain the others:
-python remix.py --ds my-podcast.ds.csv --in my-podcast.wav --exclude-speakers=2,4
+python remix.py --ds my-podcast.ds.csv --input my-podcast.wav --exclude-speakers=2,4
 
 # Either will output my-podcast.onesided.wav
 ```
--- remix.py
+++ remix.py
@@ -1,25 +1,65 @@
-import sys
+import argparse
 import csv
 from pydub import AudioSegment
 
-csv_filename = sys.argv[1]
-input_filename = sys.argv[2]
-output_filename = sys.argv[2].replace(".wav", ".onesided.wav")
+parser = argparse.ArgumentParser(
+    description="Remix a WAV file based on a diarisation CSV file"
+)
+parser.add_argument("--ds", help="CSV file with diarisation", required=True)
+parser.add_argument("--input", help="Input WAV file", required=True)
+parser.add_argument(
+    "--include-speakers",
+    nargs="+",
+    help="Speakers to include, comma-separated",
+)
+parser.add_argument(
+    "--exclude-speakers",
+    nargs="+",
+    help="Speakers to exclude, comma-separated",
+)
+args = parser.parse_args()
 
-full_audio = AudioSegment.from_wav(input_filename)
-segments = AudioSegment.empty()
+output_filename = args.input.replace(".wav", ".onesided.wav")
 
-with open(csv_filename) as csvfile:
-    reader = csv.reader(csvfile)
-    for start, end, speaker in reader:
-        if speaker in ["SPEAKER_00", "SPEAKER_00"]:
-            start = float(start) * 1000
-            end = float(end) * 1000
 
-            # Fix clipping of first sample
-            if start < 500:
-                start = 0
-            print(f"{start / float(len(full_audio))*100:.0f}%")
-            segments += full_audio[start:end]
+def main():
+    full_audio = AudioSegment.from_wav(args.input)
+    segments = AudioSegment.empty()
 
-segments.export(f"{output_filename}", format="wav")
+    with open(args.ds) as csvfile:
+        reader = csv.reader(csvfile)
+        speakers = get_speakers(reader, args.include_speakers, args.exclude_speakers)
+        csvfile.seek(0)
+
+        # Build audio segments
+        for start, end, speaker in reader:
+            if speaker in speakers:
+                start = float(start) * 1000
+                end = float(end) * 1000
+
+                # Fix clipping of first sample
+                if start < 500:
+                    start = 0
+
+                print(f"{start / float(len(full_audio))*100:.0f}%")
+                segments += full_audio[start:end]
+
+    segments.export(f"{output_filename}", format="wav")
+
+
+def get_speakers(csv_reader, include_speakers, exclude_speakers):
+    speakers = set()
+    for start, end, speaker in csv_reader:
+        speakers.add(speaker)
+    if include_speakers:
+        speakers = speakers.intersection(parse_speakers(include_speakers))
+    if exclude_speakers:
+        speakers = speakers.difference(parse_speakers(exclude_speakers))
+    return speakers
+
+
+def parse_speakers(speakers):
+    return set([f"SPEAKER_{int(s):02d}" for s in ",".join(speakers).split(",")])
+
+
+main()
